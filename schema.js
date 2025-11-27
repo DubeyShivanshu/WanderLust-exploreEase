015d9ad81476a23131e935e802a71bf0850dc3d0
Shivanshu Dubey <shivanshu17103@gmail.com>
2025-11-27 22:43:04 +0530

    Refactor schema.js for clarity and consistency

--- schema.js
+++ schema.js
@@ -1,8 +1,5 @@
-//51(G). Validation for schema data using 'JOI' (to validate the data before creating/updating a listing)
-//first install 'joi' package using 'npm i joi' -> then create a new file 'schema.js' in root dir -> then write the below code in schema.js file starting from require('joi')
 const Joi = require("joi");
 
-//defining user level schema for listing data validation using JOI
 const listingSchema = Joi.object({
     listing: Joi.object({
         title: Joi.string().required(),
@@ -13,7 +10,7 @@ const listingSchema = Joi.object({
             filename: Joi.string().required()
         }).optional(),
         
-        price: Joi.number().required().min(0),  //price should be a number, required & min value is 0(no negative price)
+        price: Joi.number().required().min(0), 
         country: Joi.string().required(),
         location: Joi.string().required(),
         category: Joi.string()
@@ -32,14 +29,11 @@ const listingSchema = Joi.object({
     }).required(),
 });
 
-//53(F). Validation for review schema data using 'JOI' (to validate the data before creating a review)
-//defining user level schema for review data validation using JOI
 const reviewSchema = Joi.object({
     review: Joi.object({
-        rating: Joi.number().required().min(1).max(5),   //rating should be a number b/w 1 to 5
-        comment: Joi.string().required(),       //comment should be a string & required
+        rating: Joi.number().required().min(1).max(5),  
+        comment: Joi.string().required(),      
     }).required(),
 });
 
-//Export listingSchema & reviewSchema both together to use in app.js file
-module.exports = {listingSchema, reviewSchema};   //exporting reviewSchema to use in app.js file
+module.exports = {listingSchema, reviewSchema};   
