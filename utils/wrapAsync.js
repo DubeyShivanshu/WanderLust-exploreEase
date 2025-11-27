958f03514d24f450b6cf1c4469ff0336f81f3d9d
Shivanshu Dubey <shivanshu17103@gmail.com>
2025-11-27 22:14:30 +0530

    Refactor wrapAsync function for error handling

--- utils/wrapAsync.js
+++ utils/wrapAsync.js
@@ -1,9 +1,5 @@
-//51(D). For wrapping async functions to catch errors and pass to next()
-// function wrapAsync(fn){      //instead of this we can use export module
-module.exports = function (fn) {      //exporting wrapAsync function module
+module.exports = function (fn) {     
     return function (req, res, next) {
         fn(req, res, next).catch(next);
     };
 };
-
-//now in app.js we can require/import this wrapAsync module and use it to wrap async functions
