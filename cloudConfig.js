f5408027ab3bfbb9b177114f13d4b5bfb9dc8774
Shivanshu Dubey <shivanshu17103@gmail.com>
2025-11-27 22:39:10 +0530

    Refactor cloudConfig.js for clarity and formatting

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,10 +1,7 @@
-//58(I). Require 'cloudinary' & 'multer-storage-cloudinary' package(firstly, 'npm i cloudinary multer-storage-cloudinary')
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-//configuring(joining) cloudinary with backend (Connect App → Cloudinary)
 cloudinary.config({
-    //join .env stored cloud codes with process.env & store in their respective vars.
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET
@@ -14,10 +11,9 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'wanderlust_DEV',    //: 'some-folder-name'
-    allowedFormats: ["png", "jpg", "jpeg"], // supports promises as well
+    folder: 'wanderlust_DEV',  
+    allowedFormats: ["png", "jpg", "jpeg"], 
   },
 });
 
-//export the two modules- cloudinary & storage to use in 'routes/listing.js'
 module.exports = { cloudinary, storage };
